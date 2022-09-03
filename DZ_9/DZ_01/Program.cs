5d9d614fc5c6b42b721a1c55d6efa17ebe3924f4
ZeN <zensmi@yandex.ru>
2022-09-03 21:38:50 +0300

    DZ

---
+++ DZ_9/DZ_01/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать натуральные числа от N до 1, N задано
+
+string NaturalNum(int n)
+     {
+         if (n == 1)
+     {
+         return "1";
+     }
+        return n + " " + NaturalNum(n - 1);
+     }
+
+ Console.WriteLine(NaturalNum(10));
