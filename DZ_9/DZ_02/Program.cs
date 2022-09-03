008807840bdc8e104697193de156cc348ddd7777
ZeN <zensmi@yandex.ru>
2022-09-03 21:45:44 +0300

    DZ

---
+++ DZ_9/DZ_02/Program.cs
@@ -0,0 +1,12 @@
+﻿//Показать натуральные числа от M до N, N и M заданы 
+
+string NaturalNum(int m, int n)
+{
+if (n == m)
+{
+    return Convert.ToString(m);
+}
+    return NaturalNum(m, n - 1) + " " + n;
+}
+
+Console.WriteLine(NaturalNum(5, 20));
