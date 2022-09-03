1ec6396c409769b5ac6f0310fdee7e50aa7889a7
ZeN <zensmi@yandex.ru>
2022-09-03 21:59:34 +0300

    DZ

---
+++ DZ_9/DZ_05/Program.cs
@@ -0,0 +1,10 @@
+﻿double Fibonacci(int n)
+{
+ if(n == 1 || n == 2) return 1;
+ else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+for (int i = 1; i < 20; i++)
+{
+
+ Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
