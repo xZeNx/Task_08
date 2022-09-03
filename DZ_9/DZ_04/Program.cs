f558250c1a4cf2f1a53c3929a766d5a5a52c62e0
ZeN <zensmi@yandex.ru>
2022-09-03 21:56:00 +0300

    DZ

---
+++ DZ_9/DZ_04/Program.cs
@@ -0,0 +1,22 @@
+﻿// Написать программу вычисления функции Аккермана
+
+int m = Numbers("Введите m: ");
+int n = Numbers("Введите n: ");
+
+int Akkerman = Ack(m, n);
+
+Console.Write($"Функция Аккермана = {Akkerman} ");
+
+int Ack(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return Ack(m - 1, 1);
+  else return Ack(m - 1, Ack(m, n - 1));
+}
+
+int Numbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
